4b1e60320eee138528d1352a7beab5c689cb33d1
Visatouch Deeying <xerodotc@gmail.com>
2020-11-02 20:14:43 +0700

    update runtime and add graceful shutdown

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,7 +20,9 @@ func main() {
 	}
 
 	r := gin.Default()
+	r.AppEngine = true
 	gin.SetMode(gin.ReleaseMode)
+
 	r.GET("/", func(c *gin.Context) {
 		c.Redirect(http.StatusFound, "https://ja.wikipedia.org/wiki/%E7%A0%82%E7%B3%96")
 	})
@@ -24,7 +30,27 @@ func main() {
 		c.Redirect(http.StatusFound, "https://myanimelist.net/character/134256/Satou_Matsuzaka")
 	})
 
-	if err := r.Run(fmt.Sprintf(":%s", port)); err != nil {
-		log.Fatal(err)
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: r,
 	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error: %v\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Error: %v\n", err)
+	}
+
+	log.Println("Exiting...")
 }
